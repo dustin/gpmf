47ce0596c8dcfeef4def0847a7a650d8ab9e9cac
Dustin Sallings <dustin@spy.net>
2020-03-15 21:49:09 -0700

    Dump out a bunch of stuff from main.

--- app/Main.hs
+++ app/Main.hs
@@ -27,12 +27,42 @@ showValues n = mapM_ showValue
 showTelemetry :: Int -> (FourCC, [Value]) -> IO ()
 showTelemetry n (FourCC (a,b,c,d),vs) =  putStrLn ((replicate n ' ') <> [a,b,c,d]) >> showValues (n+2) vs
 
+
+
 showDEVC :: DEVC -> IO ()
 showDEVC DEVC{..} = do
   putStrLn $ "Device " <> show devID <> ": " <> devName
-  mapM_ (\(k,v) -> do
-            putStrLn $ "  " <> k
-            putStrLn $ "    " <> show v) (Map.assocs devTelems)
+  mapM_ showT devTelems
+
+  where
+    showT Telemetry{..} = do
+      putStrLn $ "  " <> teleName <> " - stmp:" <> show teleStmp <> " tsmp:" <> show teleTsmp
+      showTVal teleValues
+
+    showTVal (TVUnknown vals)           = putStrLn   "    Unknown type" >> showValues 6 vals
+    showTVal (TVAccl Accelerometer{..}) = putStrLn $ "    Temp: " <> show accTemp <> " and "
+                                          <> show (length accVals) <> " values like " <> (show.head) accVals
+    showTVal (TVGyro Gyroscope{..})     = putStrLn $ "    Temp: " <> show gyroTemp <> " and "
+                                          <> show (length gyroVals) <> " values like " <> (show.head) gyroVals
+    showTVal (TVFaces fs)               = putStrLn "    Faces:" >>
+                                          mapM_ (\Face{..} -> putStrLn $ "        "
+                                                              <> show faceX <> "x" <> show faceY
+                                                              <> ", " <> show faceW <> "x" <> show faceH
+                                                              <> ", smile=" <> show faceSmile) fs
+    showTVal (TVGPS GPS{..})            = putStrLn ("    GPS time=" <> show gpsTime <> " p=" <> show gpsP
+                                                    <> ", len(rs)=" <> show (length gpsReadings)
+                                                    <> "\n        hd=" <> showGPS (head gpsReadings))
+    showTVal (TVScene ss)                = putStrLn ("   Scenes, found " <> show (length ss) <> ", first:") >>
+                                           mapM_ (\(k,v) ->
+                                                    putStrLn $ "        " <> show k <> "=" <> show v)
+                                           (Map.assocs (head ss))
+    showTVal (TVAudioLevel AudioLevel{..}) = putStrLn "    Audio levels:" >>
+                                             putStrLn ("      rms:  " <> (show audioRMS)) >>
+                                             putStrLn ("      peak: " <> (show audioPeak))
+
+showGPS :: GPSReading -> String
+showGPS GPSReading{..} = "(" <> show gpsrLat <> "," <> show gpsrLon <> ") alt=" <> show gpsrAlt
+                         <> " spd2d=" <> show gpsrSpeed2D <> " spd3d=" <> show gpsrSpeed3D
 
 main :: IO ()
 main = do
@@ -40,7 +70,7 @@ main = do
   case parseGPMF b of
     A.Fail r x y -> print (r, x,y)
     A.Done _ xs   -> do
-      let x@(f,v) = head xs
-      -- mapM_ (showDEVC . uncurry mkDEVC) xs
-      showDEVC (mkDEVC f v)
-      mapM_ (showTelemetry 0) [x]
+      -- let x@(f,v) = head xs
+      -- showDEVC (mkDEVC f v)
+      mapM_ (showDEVC . uncurry mkDEVC) xs
+      -- mapM_ (showTelemetry 0) xs
