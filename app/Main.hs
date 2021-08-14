c5582ba25df56337352a985d97d257d3d8c397ae
Dustin Sallings <dustin@spy.net>
2021-08-13 21:37:47 -0700

    Remove most of the lines of main.

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,5 @@ import           GoPro.Command.DEVCString
 
 main :: IO ()
 main = do
-  b <- BS.readFile . head =<< getArgs
-  case parseGPMF b of
-    Left y -> print y
-    Right xs   -> do
-      -- let x@(f,v) = head xs
-      -- showDEVC (mkDEVC f v)
-      mapM_ (BL.putStr . showDEVC . uncurry mkDEVC) xs
-      -- mapM_ (showTelemetry 0) xs
+  [fn] <- getArgs
+  either print (mapM_ (BL.putStrLn . showDEVC . uncurry mkDEVC)) . parseGPMF =<< BS.readFile fn
